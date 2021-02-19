364598a06ddfbbb0b5db0456a894e282b5e94081
newfacade <newfacade@163.com>
2021-02-20 05:01:23 +0800

    Boyer-Moore 投票算法

--- lists/easy_majority_element.py
+++ lists/easy_majority_element.py
@@ -5,7 +5,10 @@ class Solution:
     """
     给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素
     Boyer-Moore 投票算法
-    可简化为众数和其他类
+    if final_candidate != major:
+    1. (major, *) period eliminated, each (major, major) correspond to a (major, other)
+    2. (other, major) correspond to a (other, other)
+    so #(major) = #(major, major) + #(other, major) <= #(major, other) + #(other, other) = #(other), contradiction!
     """
     def majorityElement(self, nums: List[int]) -> int:
         count = 0

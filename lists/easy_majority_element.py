0a82279d2a6ad37acf1e52030dda729d80ba286c
newfacade <newfacade@163.com>
2021-02-20 03:59:14 +0800

    add lists

---
+++ lists/easy_majority_element.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    """
+    给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素
+    Boyer-Moore 投票算法
+    可简化为众数和其他类
+    """
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
